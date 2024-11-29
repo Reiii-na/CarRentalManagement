8fb227dff6438ae1427c7e4555c6aa724bff5732
PHAN BAO NGOC 2302853C <2302853C@student.tp.edu.sg>
2024-11-29 12:11:52 +0800

    saving

--- CarRentalManagement/Data/CarRentalManagementContext.cs
+++ CarRentalManagement/Data/CarRentalManagementContext.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using CarRentalManagement.Data;
 using CarRentalManagement.Configuration.Entities;
 
 namespace CarRentalManagement.Data
